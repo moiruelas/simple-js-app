20b5643c8b318bf3243107065c783baa9dd1e0de
moiruelas <140809258+moiruelas@users.noreply.github.com>
2024-05-08 19:16:58 -0700

    Added Pokedex Variables and Arrays
    Created pokedex variables and added arrays for the first 3 starters and their full evolutions

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
-alert('Hello World');
-
-let favoriteFood = 'Pizza';
-document.write('Pizza');
+let pokemonList = [
+  {name: "Bulbasaur", height: 0.7, type: ['Grass', 'Poison']},
+  {name: "Ivysaur", height: 1, type: ['Grass', 'Poison']},
+  {name: "Venusaur", height: 2, type: ['Grass', 'Poison']},
+  {name: "Charmander", height: 0.6, type: ['Fire']},
+  {name: "Charmeleon", height: 1.1, type: ['Fire']},
+  {name: "Charizard", height: 1.7, type: ['Fire', 'Flying']},
+  {name: "Squirtle", height: 0.5, type: ['Water']},
+  {name: "Wartortle", height: 1, type: ['Water']},
+  {name: "Blastoise", height: 1.6, type: ['Water']},
+];
