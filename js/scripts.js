c6f974044fa7d40959cbd58b69eb887fc21aa7b3
moiruelas <140809258+moiruelas@users.noreply.github.com>
2024-06-28 15:46:43 -0700

    Removed color styling, added promise polyfill, and updated pokedex to use an API
    Removed unused styling, added promise polyfill.
    
    In the JS sheet, I removed the pokemon array and replaced with an api. I also added functions to load show details from the api when called and to create the pokedex as buttons and display in the console when one is cliked

--- js/scripts.js
+++ js/scripts.js
@@ -1,159 +1,8 @@
 let pokemonRepository = (function() {
   // This is an immediately invoked function expression (IIFE)
-  let pokemonList = [
+  let pokemonList = [];
     //let pokemonList makes it so variable is named pokemonList. The [] is used to create an array. Each item inside the array that is also nested inside {} is an object. The name of the object denoted as Name: followed by "bulbasuar" indicates that the name of the object is bulbasaur, this is the key of the preceding item, height is an additional key named height folloed by the height of the object, keys and values are denoted by the colon. Type is another key that is also an additional array containing the type of the pokemo in strings denoted by the apostrophe.
-    {name: "Bulbasaur", height: 0.7, type: ['Grass', 'Poison']},
-    {name: "Ivysaur", height: 1, type: ['Grass', 'Poison']},
-    {name: "Venusaur", height: 2, type: ['Grass', 'Poison']},
-    {name: "Charmander", height: 0.6, type: ['Fire']},
-    {name: "Charmeleon", height: 1.1, type: ['Fire']},
-    {name: "Charizard", height: 1.7, type: ['Fire', 'Flying']},
-    {name: "Squirtle", height: 0.5, type: ['Water']},
-    {name: "Wartortle", height: 1, type: ['Water']},
-    {name: "Blastoise", height: 1.6, type: ['Water']},
-    {name: "Caterpie", height: 0.3, type: ['Bug']},
-    {name: "Metapod", height: 0.7, type: ['Bug']},
-    {name: "Butterfree", height: 1.1, type: ['Bug', 'Flying']},
-    {name: "Weedle", height: 0.3, type: ['Bug', 'Poison']},
-    {name: "Kakuna", height: 0.6, type: ['Bug', 'Poison']},
-    {name: "Beedrill", height: 1, type: ['Bug', 'Poison']},
-    {name: "Pidgey", height: 0.3, type: ['Normal', 'Flying']},
-    {name: "Pidgeotto", height: 1.1, type: ['Normal', 'Flying']},
-    {name: "Pidgeot", height: 1.5, type: ['Normal', 'Flying']},
-    {name: "Rattata", height: 0.3, type: ['Normal']},
-    {name: "Raticate", height: 0.7, type: ['Normal']},
-    {name: "Spearow", height: 0.3, type: ['Normal', 'Flying']},
-    {name: "Fearow", height: 1.2, type: ['Normal', 'Flying']},
-    {name: "Ekans", height: 2, type: ['Poison']},
-    {name: "Arbok", height: 3.5, type: ['Poison']},
-    {name: "Pikachu", height: 0.4, type: ['Electric']},
-    {name: "Raichu", height: 0.8, type: ['Electric']},
-    {name: "Sandshrew", height: 0.6, type: ['Ground']},
-    {name: "Sandslash", height: 1, type: ['Ground']},
-    {name: "Nidoran♀", height: 0.4, type: ['Poison']},
-    {name: "Nidorina", height: 0.8, type: ['Poison']},
-    {name: "Nidoqueen", height: 1.3, type: ['Poison', 'Ground']},
-    {name: "Nidoran♂", height: 0.5, type: ['Poison']},
-    {name: "Nidorino", height: 0.9, type: ['Poison']},
-    {name: "Nidoking", height: 1.4, type: ['Poison', 'Ground']},
-    {name: "Clefairy", height: 0.6, type: ['Fairy']},
-    {name: "Clefable", height: 1.3, type: ['Fairy']},
-    {name: "Vulpix", height: 0.6, type: ['Fire']},
-    {name: "Ninetales", height: 1.1, type: ['Fire']},
-    {name: "Jigglypuff", height: 0.5, type: ['Normal', 'Fairy']},
-    {name: "Wigglytuff", height: 1, type: ['Normal', 'Fairy']},
-    {name: "Zubat", height: 0.8, type: ['Poison', 'Flying']},
-    {name: "Golbat", height: 1.6, type: ['Poison', 'Flying']},
-    {name: "Oddish", height: 0.5, type: ['Grass', 'Poison']},
-    {name: "Gloom", height: 0.8, type: ['Grass', 'Poison']},
-    {name: "Vileplume", height: 1.2, type: ['Grass', 'Poison']},
-    {name: "Paras", height: 0.3, type: ['Bug', 'Grass']},
-    {name: "Parasect", height: 1, type: ['Bug', 'Grass']},
-    {name: "Venonat", height: 1, type: ['Bug', 'Poison']},
-    {name: "Venomoth", height: 1.5, type: ['Bug', 'Poison']},
-    {name: "Diglett", height: 0.2, type: ['Ground']},
-    {name: "Dugtrio", height: 0.7, type: ['Ground']},
-    {name: "Meowth", height: 0.4, type: ['Normal']},
-    {name: "Persian", height: 1, type: ['Normal']},
-    {name: "Psyduck", height: 0.8, type: ['Water']},
-    {name: "Golduck", height: 1.7, type: ['Water']},
-    {name: "Mankey", height: 0.5, type: ['Fighting']},
-    {name: "Primeape", height: 1, type: ['Fighting']},
-    {name: "Growlithe", height: 0.8, type: ['Fire']},
-    {name: "Arcanine", height: 1.9, type: ['Fire']},
-    {name: "Poliwag", height: 0.6, type: ['Water']},
-    {name: "Poliwhirl", height: 1, type: ['Water']},
-    {name: "Poliwrath", height: 1.3, type: ['Water', 'Fighting']},
-    {name: "Abra", height: 0.9, type: ['Psychic']},
-    {name: "Kadabra", height: 1.3, type: ['Psychic']},
-    {name: "Alakazam", height: 1.5, type: ['Psychic']},
-    {name: "Machop", height: 0.8, type: ['Fighting']},
-    {name: "Machoke", height: 1.5, type: ['Fighting']},
-    {name: "Machamp", height: 1.6, type: ['Fighting']},
-    {name: "Bellsprout", height: 0.7, type: ['Grass', 'Poison']},
-    {name: "Weepinbell", height: 1, type: ['Grass', 'Poison']},
-    {name: "Victreebel", height: 1.7, type: ['Grass', 'Poison']},
-    {name: "Tentacool", height: 0.9, type: ['Water', 'Poison']},
-    {name: "Tentacruel", height: 1.6, type: ['Water', 'Poison']},
-    {name: "Geodude", height: 0.4, type: ['Rock', 'Ground']},
-    {name: "Graveler", height: 1, type: ['Rock', 'Ground']},
-    {name: "Golem", height: 1.4, type: ['Rock', 'Ground']},
-    {name: "Ponyta", height: 1, type: ['Fire']},
-    {name: "Rapidash", height: 1.7, type: ['Fire']},
-    {name: "Slowpoke", height: 1.2, type: ['Water', 'Psychic']},
-    {name: "Slowbro", height: 1.6, type: ['Water', 'Psychic']},
-    {name: "Magnemite", height: 0.3, type: ['Electric', 'Steel']},
-    {name: "Magneton", height: 1, type: ['Electric', 'Steel']},
-    {name: "Farfetch'd", height: 0.8, type: ['Normal', 'Flying']},
-    {name: "Doduo", height: 1.4, type: ['Normal', 'Flying']},
-    {name: "Dodrio", height: 1.8, type: ['Normal', 'Flying']},
-    {name: "Seel", height: 1.1, type: ['Water']},
-    {name: "Dewgong", height: 1.7, type: ['Water', 'Ice']},
-    {name: "Grimer", height: 0.9, type: ['Poison']},
-    {name: "Muk", height: 1.2, type: ['Poison']},
-    {name: "Shellder", height: 0.3, type: ['Water']},
-    {name: "Cloyster", height: 1.5, type: ['Water', 'Ice']},
-    {name: "Gastly", height: 1.3, type: ['Ghost', 'Poison']},
-    {name: "Haunter", height: 1.6, type: ['Ghost', 'Poison']},
-    {name: "Gengar", height: 1.5, type: ['Ghost', 'Poison']},
-    {name: "Onix", height: 8.8, type: ['Rock', 'Ground']},
-    {name: "Drowzee", height: 1, type: ['Psychic']},
-    {name: "Hypno", height: 1.6, type: ['Psychic']},
-    {name: "Krabby", height: 0.4, type: ['Water']},
-    {name: "Kingler", height: 1.3, type: ['Water']},
-    {name: "Voltorb", height: 0.5, type: ['Electric']},
-    {name: "Electrode", height: 1.2, type: ['Electric']},
-    {name: "Exeggcute", height: 0.4, type: ['Grass', 'Psychic']},
-    {name: "Exeggutor", height: 2, type: ['Grass', 'Psychic']},
-    {name: "Cubone", height: 0.4, type: ['Ground']},
-    {name: "Marowak", height: 1, type: ['Ground']},
-    {name: "Hitmonlee", height: 1.5, type: ['Fighting']},
-    {name: "Hitmonchan", height: 1.4, type: ['Fighting']},
-    {name: "Lickitung", height: 1.2, type: ['Normal']},
-    {name: "Koffing", height: 0.6, type: ['Poison']},
-    {name: "Weezing", height: 1.2, type: ['Poison']},
-    {name: "Rhyhorn", height: 1, type: ['Ground', 'Rock']},
-    {name: "Rhydon", height: 1.9, type: ['Ground', 'Rock']},
-    {name: "Chansey", height: 1.1, type: ['Normal']},
-    {name: "Tangela", height: 1, type: ['Grass']},
-    {name: "Kangaskhan", height: 2.2, type: ['Normal']},
-    {name: "Horsea", height: 0.4, type: ['Water']},
-    {name: "Seadra", height: 1.2, type: ['Water']},
-    {name: "Goldeen", height: 0.6, type: ['Water']},
-    {name: "Seaking", height: 1.3, type: ['Water']},
-    {name: "Staryu", height: 0.8, type: ['Water']},
-    {name: "Starmie", height: 1.1, type: ['Water', 'Psychic']},
-    {name: "Mr. Mime", height: 1.3, type: ['Psychic', 'Fairy']},
-    {name: "Scyther", height: 1.5, type: ['Bug', 'Flying']},
-    {name: "Jynx", height: 1.4, type: ['Ice', 'Psychic']},
-    {name: "Electabuzz", height: 1.1, type: ['Electric']},
-    {name: "Magmar", height: 1.3, type: ['Fire']},
-    {name: "Pinsir", height: 1.5, type: ['Bug']},
-    {name: "Tauros", height: 1.4, type: ['Normal']},
-    {name: "Magikarp", height: 0.9, type: ['Water']},
-    {name: "Gyarados", height: 6.5, type: ['Water', 'Flying']},
-    {name: "Lapras", height: 2.5, type: ['Water', 'Ice']},
-    {name: "Ditto", height: 0.3, type: ['Normal']},
-    {name: "Eevee", height: 0.3, type: ['Normal']},
-    {name: "Vaporeon", height: 1, type: ['Water']},
-    {name: "Jolteon", height: 0.8, type: ['Electric']},
-    {name: "Flareon", height: 0.9, type: ['Fire']},
-    {name: "Porygon", height: 0.8, type: ['Normal']},
-    {name: "Omanyte", height: 0.4, type: ['Rock', 'Water']},
-    {name: "Omastar", height: 1, type: ['Rock', 'Water']},
-    {name: "Kabuto", height: 0.5, type: ['Rock', 'Water']},
-    {name: "Kabutops", height: 1.3, type: ['Rock', 'Water']},
-    {name: "Aerodactyl", height: 1.8, type: ['Rock', 'Flying']},
-    {name: "Snorlax", height: 2.1, type: ['Normal']},
-    {name: "Articuno", height: 1.7, type: ['Ice', 'Flying']},
-    {name: "Zapdos", height: 1.6, type: ['Electric', 'Flying']},
-    {name: "Moltres", height: 2, type: ['Fire', 'Flying']},
-    {name: "Dratini", height: 1.8, type: ['Dragon']},
-    {name: "Dragonair", height: 4, type: ['Dragon']},
-    {name: "Dragonite", height: 2.2, type: ['Dragon', 'Flying']},
-    {name: "Mewtwo", height: 2, type: ['Psychic']},
-    {name: "Mew", height: 0.4, type: ['Psychic']},
-  ];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=151';
 
   function addListItem(pokemon, isTallest) {
     //This function is named addListItem and takes in two parameters, pokemon and isTallest (this indicates if the pokemon is the tallest).
@@ -163,7 +12,8 @@ let pokemonRepository = (function() {
     //This creates a new DOM element with the tag name `li` and assigns it to the variable named `listpokemon`. It will be used to contain the name, height and type of the pokemon, it creates one for each pokemon.
     let button = document.createElement("button");
     //This creates a new button element and assigns it to the variable named `button`. Each pokemon will have a button with the name, height and type of the pokemon.
-    button.innerText = `${pokemon.name} (Height: ${pokemon.height}m, Type: ${pokemon.type.join(', ')})`;
+    button.innerText = `${pokemon.name}` 
+    //(Height: ${pokemon.height}m, Type: ${pokemon.type.join(', ')})`;
     //This adds the name, height and type of the pokemon to the button.
     if (isTallest) {
       button.innerText += " - Wow, that's a big Pokémon!";
@@ -181,40 +31,93 @@ let pokemonRepository = (function() {
   }
   //This adds the button to the pokemon list in the DOM (or webpage). This allows it to be displayed on the screen.
 
-  function showDetails(pokemon) {
-    //This defines the function showDetails, which takes one parameter in this case it is called pokemon.
-    console.log(`Name: ${pokemon.name}`);
-    //This logs the name of the pokemon in the console.
-    console.log(`Height: ${pokemon.height}`);
-    //This logs the height of the pokemon in the console.
-    console.log(`Type: ${pokemon.type.join(', ')}`);
-    //This logs the type of the pokemon in the console. I
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url,
+          height: item.height,
+          type: [item.types]
+        };
+        add(pokemon);
+        console.log(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
   }
+
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      //Now we add the details to the item
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+  
+  function showDetails(item) {
+    pokemonRepository.loadDetails(item).then(function ()
+    {
+      console.log(item);
+    });
+  }
+
+  // function showDetails(pokemon) {
+  //   //This defines the function showDetails, which takes one parameter in this case it is called pokemon.
+  //   console.log(`Name: ${pokemon.name}`);
+  //   //This logs the name of the pokemon in the console.
+  //   console.log(`Height: ${pokemon.height}`);
+  //   //This logs the height of the pokemon in the console.
+  //   console.log(`Type: ${pokemon.type.join(', ')}`);
+  //   //This logs the type of the pokemon in the console. I
+  // }
   //Initially this code was just written as console.log(pokemon); However when logging to the conosle it was logging the types as "Array(1)or(2)" for the number of types in the array instead of the actual type. I changed it to print the exact text to actually log the type.
 
-  return {
-    getAll: function() {
-      return pokemonList;
-      //This function returns the pokemon list.
-    },
+  
+  function add(item) {
+    //This funcction allows the user to add a pokemon to the list array.
+    if (typeof item === 'object' && "name" in item && "detailsUrl" in item) {
+    //This checks that the item added is an object with name, height and type.
+      pokemonList.push(item);
+      //The push method adds the new pokemon to the list array.
+      console.log (`${item.name} added to pokemonList.`);
+      //This logs in the console that the pokemon has been added correctly.
+    } else {
+      console.error(`Invalid Pokemon. Please check your Pokemon name, height and type.`);
+      //This logs in the conosle if there was an errr in adding the pokemon to the list and advoces to check for errors. I will later update this to be able to identify what exactly did not work.
+    }
+  }
 
-    add: function(item) {
-      //This funcction allows the user to add a pokemon to the list array.
-      if (typeof item === 'object' && item.name && item.height && item.type) {
-      //This checks that the item added is an object with name, height and type.
-        pokemonList.push(item);
-        //The push method adds the new pokemon to the list array.
-        console.log (`${item.name} added to pokemonList.`);
-        //This logs in the console that the pokemon has been added correctly.
-      } else {
-        console.error(`Invalid Pokemon. Please check your Pokemon name, height and type.`);
-        //This logs in the conosle if there was an errr in adding the pokemon to the list and advoces to check for errors. I will later update this to be able to identify what exactly did not work.
-      }
-    },
+  function getAll() {
+    return pokemonList;
+    //This function returns the pokemon list.
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
     addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
     showDetails: showDetails
   };
 })();
+
+pokemonRepository.loadList().then(function() {
+  // Now the data is loaded!
+  pokemonRepository.getAll().forEach(function(pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
+});
 
 let tallestPokemon = null;
 let tallestHeight = 0;
@@ -237,30 +140,3 @@ pokemonRepository.getAll().forEach(pokemon => {
   //This loop runs through the entire list of pokemon and calls the addListItem function for each pokemon.
   //This variable will be used to display the pokemon on the screen.
 });
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-function divide(dividend, divisor) {
-  if (divisor === 0) {
-    return "You're trying to divide by zero";
-  } else {
-    let result = dividend / divisor;
-    return result;
-  }
-}
-
-console.log(divide(4, 2));
-console.log(divide(7, 0));
-console.log(divide(1, 4));
-console.log(divide(12, -3));
-*/
