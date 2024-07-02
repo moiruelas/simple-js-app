ed53548e4e644c968192f88425dabdeafd3a2f70
moiruelas <140809258+moiruelas@users.noreply.github.com>
2024-07-01 17:19:14 -0700

    Styling for buttons, added div modal container, and added modal functionality
    - Added modal and button styling
    - Added div for modal
    - Edited various code and removed unnecessary comments
    - Added new comments
    - Added modals for each pokemon
    - Added event listeners so when a button is clicked it opens a modal for that pokemon
    - Added functions to display pokemon name, height and img inside the modal
    - Added close modal functionalities to close via esc, close button, and clicking outside the modal
    - Removed logging the pokemon to the console when clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,20 @@
 let pokemonRepository = (function() {
   // This is an immediately invoked function expression (IIFE)
   let pokemonList = [];
-    //let pokemonList makes it so variable is named pokemonList. The [] is used to create an array. Each item inside the array that is also nested inside {} is an object. The name of the object denoted as Name: followed by "bulbasuar" indicates that the name of the object is bulbasaur, this is the key of the preceding item, height is an additional key named height folloed by the height of the object, keys and values are denoted by the colon. Type is another key that is also an additional array containing the type of the pokemo in strings denoted by the apostrophe.
-  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=151';
+    //This empty array denoted by [] will be filled out from the API below by the functions below. Mainly by addListItem and loadList.
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=151';//This sets the URL for the API.
 
   function addListItem(pokemon, isTallest) {
     //This function is named addListItem and takes in two parameters, pokemon and isTallest (this indicates if the pokemon is the tallest).
     let pokemonListElement = document.querySelector(".pokemon-list");
     //This finds the DOM elemnt with the class named `.pokemon-list` and assigns to the variable named `pokemonListElement`.
     let listpokemon = document.createElement("li");
-    //This creates a new DOM element with the tag name `li` and assigns it to the variable named `listpokemon`. It will be used to contain the name, height and type of the pokemon, it creates one for each pokemon.
+    //This creates a new DOM element with the tag name `li` and assigns it to the variable named `listpokemon`. 
+    //It will be used to contain the name, height and type of the pokemon, it creates one for each pokemon.
     let button = document.createElement("button");
-    //This creates a new button element and assigns it to the variable named `button`. Each pokemon will have a button with the name, height and type of the pokemon.
-    button.innerText = `${pokemon.name}` 
+    //This creates a new button element and assigns it to the variable named `button`. 
+    //Each pokemon will have a button with the name, height and type of the pokemon.
+    button.innerText = `${pokemon.name}`; 
     //(Height: ${pokemon.height}m, Type: ${pokemon.type.join(', ')})`;
     //This adds the name, height and type of the pokemon to the button.
     if (isTallest) {
@@ -32,56 +34,150 @@ let pokemonRepository = (function() {
   //This adds the button to the pokemon list in the DOM (or webpage). This allows it to be displayed on the screen.
 
   function loadList() {
+    //This function loads the list of pokemons from the API.
     return fetch(apiUrl).then(function (response) {
+      //This starts a fetch request to the API, the function then returns the promise that resolves to response of the reuqest. 
+      //Once the promise resolves, the response is passed to the next .then(). 
       return response.json();
+      //This returns the response in JSON format
     }).then(function (json) {
+      //This takes the JSON and converts it to an object
       json.results.forEach(function (item) {
+        //This iterates through the JSON object and adds each pokemon to the list
         let pokemon = {
+          //This creates an object that will be added to the list
           name: item.name,
+          //This adds the name of the pokemon from the api
           detailsUrl: item.url,
-          height: item.height,
-          type: [item.types]
+          //This adds the url of the pokemon from the api
         };
         add(pokemon);
-        console.log(pokemon);
+        //This calls the add function to add the pokemon to the list
       });
     }).catch(function (e) {
+      //If any errors occur during the fetch request, they will be caught here. The error will be passed this this function as the 'e' parameter.
       console.error(e);
+      //This logs the error in the console
     })
   }
 
   function loadDetails(item) {
+    //This function will load the details of the pokemon from the API
     let url = item.detailsUrl;
+    //This returns the url of the pokemon with it's details and assigns it to the variable 'url'
     return fetch(url).then(function (response) {
+      //This starts a fetch request to the url of the pokemon that then resolves the response of the request. 
+      //Once resolved the response object is passed to the next .then()
       return response.json();
+      //This returns the response in JSON format and returns it as a promise. Once resolved the JSON object is passed to the next .then()
     }).then(function (details) {
+      //This takes the JSON and converts it to an object
       //Now we add the details to the item
       item.imageUrl = details.sprites.front_default;
+      //This adds the image url to the item, allowing an img to be displayed
       item.height = details.height;
+      //This adds the height to the item
       item.types = details.types;
+      //This adds the types to the pokemon
     }).catch(function (e) {
       console.error(e);
+      //This logs the error in the console as an 'e' parameter
     });
   }
   
   function showDetails(item) {
-    pokemonRepository.loadDetails(item).then(function ()
-    {
-      console.log(item);
+    //This function shows the details of the pokemon
+    pokemonRepository.loadDetails(item).then(function (){
+      //This calls the loadDetails function from the repository and passes the item as the parameter. 
+      //Then it returns a promise that resolves when the details are loaded and added to the pokemon.
+      //Once the promise is resolved, the next .then() is called.
+      showModal(item.name, `Height: ${item.height}m`, item.imageUrl);
+      //This calls the showModal function which displahys the pokemon's details in a modal dialog.
+      //It then passes three arguments whigh are the name, height and image url.
     });
   }
 
-  // function showDetails(pokemon) {
-  //   //This defines the function showDetails, which takes one parameter in this case it is called pokemon.
-  //   console.log(`Name: ${pokemon.name}`);
-  //   //This logs the name of the pokemon in the console.
-  //   console.log(`Height: ${pokemon.height}`);
-  //   //This logs the height of the pokemon in the console.
-  //   console.log(`Type: ${pokemon.type.join(', ')}`);
-  //   //This logs the type of the pokemon in the console. I
-  // }
-  //Initially this code was just written as console.log(pokemon); However when logging to the conosle it was logging the types as "Array(1)or(2)" for the number of types in the array instead of the actual type. I changed it to print the exact text to actually log the type.
+  function showModal(title, text, imageUrl) {
+    //This function displays the details of the pokemon in a modal dialog (name as title, height as text and image url).
+    let modalContainer = document.querySelector('#modal-container');
+    //This selects the HTML element with the ID 'modal-container' and assigns it to the variable 'modalContainer'.
+    modalContainer.innerHTML = '';
+    //This clears any content that is inside the modalContainer
 
+    let modal = document.createElement('div');
+    //This creates a new 'div' and assigns it to the variable 'modal'
+    modal.classList.add('modal');
+    //This adds the class 'modal' to the new 'div'
+
+    let closeButtonElement = document.createElement('button');
+    //This creates a new 'button' and assigns it to the variable 'closeButtonElement'
+    closeButtonElement.classList.add('modal-close');
+    //This adds the class 'modal-close' to the new 'button' which then allows css styling to be applied
+    closeButtonElement.innerText = 'Close';
+    //This adds the text 'Close' to the new 'button'
+    closeButtonElement.addEventListener('click', hideModal);
+    //This adds an eventListener to the new 'button' that hides the modal effectively closing it when clicked
+
+    let titleElement = document.createElement('h1');
+    //This creates a new 'h1' and assigns it to the variable 'titleElement'
+    titleElement.innerText = title;
+    //This adds the text 'title' to the new 'h1'
+
+    let contentElement = document.createElement('p');
+    //This  creates a new 'p' and assigns it to the variable 'contentElement'
+    contentElement.innerText = text;
+    //  This adds the text 'text' to the new 'p'
+
+    let imageElement = document.createElement('img');
+    //This creates a new 'img' and assigns it to the variable 'imageElement'
+    imageElement.src = imageUrl;
+    //This adds the image url to the new 'img'
+    imageElement.alt = title;
+    //This selts the alt text title
+    imageElement.classList.add('modal-image'); //Adding a class for styke sheet
+
+    modal.appendChild(closeButtonElement);
+    //This appends the new 'button' to the modal
+    modal.appendChild(titleElement);
+    //This appends the new 'h1' to the modal
+    modal.appendChild(contentElement);
+    //This appends the new 'p' to the modal
+    modal.appendChild(imageElement);
+    //This appends the new 'imgElement' to the modal
+    modalContainer.appendChild(modal);
+    //This appends the new 'modal' to the modalContainer
+
+    modalContainer.classList.add('is-visible');
+    //This adds the class 'is-visible' to the modalContainer to make it visible
+
+    document.addEventListener('keydown', function(e) {
+      //This listens for the keydown event
+      if (e.key === 'Escape') {
+        //This checks if the key pressed is 'Escape'
+        hideModal();
+        //This hides the modal when the 'Escape' key is pressed
+      }
+    });
+
+    modalContainer.addEventListener('click', (e) => {
+      //This adds an event listener to the modalContainer that listens for a click
+      let target = e.target;
+      //This assigns the click event to the variable 'target'
+      if (target === modalContainer) {
+        //This checks if the target is the modalContainer
+        hideModal();
+        //This hides the modal once clicked outside of it
+      }
+    });
+  }
+
+  function hideModal() {
+    //This function hides the modal by removing the class 'is-visible' from the modalContainer
+    let modalContainer = document.querySelector('#modal-container');
+    //This selects the HTML element with the ID 'modal-container' and assigns it to the variable 'modalContainer'.
+    modalContainer.classList.remove('is-visible');
+    //This removes the class 'is-visible' from the modalContainer to make it invisible using CSS styling
+  }
   
   function add(item) {
     //This funcction allows the user to add a pokemon to the list array.
@@ -103,19 +199,28 @@ let pokemonRepository = (function() {
   }
 
   return {
+    //This begins the return of an object from the IIFE, it contains the below methods that can be accessed from the outside of the IIFE
     add: add,
+    //This adds the 'add' method to the object
     getAll: getAll,
+    //This adds the 'getAll' method to the object
     addListItem: addListItem,
+    //This adds the 'addListItem' method to the object
     loadList: loadList,
+    //This adds the 'loadList' method to the object
     loadDetails: loadDetails,
+    //This adds the 'loadDetails' method to the object
     showDetails: showDetails
+    //This adds the 'showDetails' method to the object
   };
 })();
 
 pokemonRepository.loadList().then(function() {
-  // Now the data is loaded!
+  //This calls the loadList function from the repository, once the list is loaded it will call the next .then()
   pokemonRepository.getAll().forEach(function(pokemon) {
+    //This iterates through the list and adds each pokemon to the list, once each pokemon is added it will call the next .then()
     pokemonRepository.addListItem(pokemon);
+    //This calls the addListItem function from the repository and a new item and button for the pokemon adds it to the DOM making it visible
   });
 });
 
